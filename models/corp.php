91e811f681e2cf07fac9f7e58e8840648848b00f
miriku <me@miriku.com>
2018-06-13 16:09:54 +0000

    fleshing out model classes

--- models/corp.php
+++ models/corp.php
@@ -2,5 +2,19 @@
 	class Corp
 	{
 		public $id;
+		public $name;
+		public $ceo; // class supervisor
+		public $division; // division object
+		
+		public function __construct()
+		{
+			// make name
+			// nameGenerator + Inc / Co / & Sons / Ltd / Gmbh / Partnership / Cartel / Trust
+			// this arrives from config array in harvest breaker
+		}
+
+		public function calculateProfit()
+		{
+		}
 	}
 	
